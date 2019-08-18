5814909b15fc19f7797f63351df2760ba2bc79d0
Sellieee <sellieee@hotmail.com>
2019-08-18 18:22:47 +1000

    fixed minor issues in initial files

--- javascript/liri.js
+++ javascript/liri.js
@@ -1,17 +1,17 @@
-``
-`js
+// Requiring and configuring dotenv
 require("dotenv").config();
-`
-``
 
-``
-`js
+// Use Spotify API to get keys from keys.js
 var keys = require("./javascript/keys.js");
-`
-``
-
-``
-`js
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
-`
-``
+
+// Moment.js
+var moment = require("moment")
+moment().format();
+
+// Using Axios to get information from APIs for OMDB and concert-this
+var axios = require("axios");
+
+// Read random.txt file for the do-what-it-says
+var fs = require("fs");
